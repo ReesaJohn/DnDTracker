21340f0564c157f6a49c20259cef02a2dcd92c64
Brandon Lyday <bwlyday@gmail.com>
2020-03-01 01:43:40 -0800

    MVP API created

--- backend/CombatTracker/api/urls.py
+++ backend/CombatTracker/api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from .views import get_combatants
+from .views import *
 
 urlpatterns = [
-    path('api/get_combatants', get_combatants)
+    path('api/get_user_creations', get_user_creations),
+    path('api/get_monster_names', get_monster_names),
+    path('api/get_monster', get_monster),
+    # path('api/get_all_monsters', get_monsters)
 ]
--- backend/CombatTracker/api/views.py
+++ backend/CombatTracker/api/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
-from rest_framework import generics, status
 from django.http import HttpResponse
 from django.http import JsonResponse
-from django.views import View
+from rest_framework import status
+from rest_framework.exceptions import NotFound
+
+import urllib.request
+import json
 
 from .models import Combatant
 from .serializers import CombatantSerializer
 
 
-def get_combatants(request):
+def get_user_creations(request):
     combatants = Combatant.objects.all()
     serializer_data = CombatantSerializer(combatants, many=True).data
     return JsonResponse(
@@ -17,4 +20,58 @@ def get_combatants(request):
         },
         status=status.HTTP_200_OK,
     )
+
+
+def get_monster_names(request):
+    url = 'http://dnd5eapi.co/api/monsters/'
+    serialized_data = urllib.request.urlopen(url=url).read()
+    data = json.loads(serialized_data)
+    results = data["results"]
+
+    monsters = {"monsters": []}
+    for result in results:
+        monsters["monsters"].append({
+            "index": result["index"],
+            "monster_name": result["name"]
+        })
+
+    return JsonResponse(monsters, status=status.HTTP_200_OK)
+
+
+def get_monster(request):
+    monster_name = request.GET.get("monster")
+    if monster_name is None:
+        raise NotFound
+
+    monster_name = monster_name.lower()
+    monster_name = monster_name.replace(' ', '-', monster_name.count(' '))
+    url = 'http://dnd5eapi.co/api/monsters/' + monster_name
+    serialized_data = urllib.request.urlopen(url=url).read()
+    data = json.loads(serialized_data)
+
+    ac = data["armor_class"]
+    max_hp = data["hit_points"]
+    return JsonResponse({
+        "name": monster_name,
+        "ac": ac,
+        "max_hp": max_hp
+    }, status=status.HTTP_200_OK)
+
+
+# def get_monsters(request):
+#     monsters_url = 'http://dnd5eapi.co/api/monsters/'
+#     serialized_data = urllib.request.urlopen(url=monsters_url).read()
+#     data = json.loads(serialized_data)
+#     results = data["results"]
+#
+#     monsters = {"monsters": []}
+#     for result in results:
+#         index = result["index"]
+#         serialized_monster_data = urllib.request.urlopen(url=(monsters_url + index)).read()
+#         monster_data = json.loads(serialized_monster_data)
+#         monsters["monsters"].append({
+#             "name": monster_data["name"],
+#             "ac": monster_data["armor_class"],
+#             "max_hp": monster_data["hit_points"]
+#         })
 
